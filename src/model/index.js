d7bcadcc24ae45ab4f7380c8156954085cfb3cfd
test <test@gmail.com>
2025-04-23 16:14:29 +0530

    test author

--- src/model/index.js
+++ src/model/index.js
@@ -1,10 +1,20 @@
 const { Sequelize, DataTypes } = require('sequelize');
 const dbConfig = require('../config/db.config');
 
-const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
-  host: dbConfig.HOST,
-  dialect: dbConfig.DIALECT,
-});
+const sequelize = new Sequelize(
+  dbConfig.DB,
+  dbConfig.USER,
+  dbConfig.PASSWORD,
+  {
+    host: dbConfig.HOST,
+    dialect: dbConfig.DIALECT,
+    port: dbConfig.PORT,
+    dialectOptions: {
+      connectTimeout: 10000, // 10 seconds
+    },
+    logging: false,
+  }
+);
 
 const db = {};
 db.Sequelize = Sequelize;
